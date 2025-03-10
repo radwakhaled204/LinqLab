ad4dd6f5e0d55188a79938a9d2f70b1df5687077
Radwa Khaled <radwaabouzed@gmail.com>
2025-03-10 21:54:34 +0200

    add new fun to program

--- LinqLab/Program.cs
+++ LinqLab/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using SeedData;
+using SeedData.Models;
+using System.Linq;
+
+
+var cats = GetData.GetCategories(GetData.GetCustomers());
+
+//var list = cats.Select(c => c.customer);
+//foreach (var c in list)
+//{
+//    foreach (var cust in c)
+//    {
+//        Console.WriteLine(cust.id + " : " + cust.name);
+//    }
+//}
+
+
+var list = cats.SelectMany(c => c.customer);
+foreach (var c in list)
+{
+    Console.WriteLine(c.id + " : " + c.name);
+}
+
