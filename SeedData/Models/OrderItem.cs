9caefc8469c44313e12053e2ea840340dbb45bca
Radwa Khaled <radwaabouzed@gmail.com>
2025-03-12 22:32:45 +0200

    add data annotion

--- SeedData/Models/OrderItem.cs
+++ SeedData/Models/OrderItem.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,9 @@ namespace SeedData.Models
 {
     public class OrderItem
     {
+        [Key]
         public int Id { get; set; }
+        [ForeignKey(nameof(OrderItem.Id))]
         public int OrderId { get; set; }
         [Required]
         public string ItemName { get; set; }
