677549d78dd62adf699cd5378a6c41705e0d7307
Radwa Khaled <radwaabouzed@gmail.com>
2025-03-12 22:28:05 +0200

    add data annotion

--- SeedData/Models/Order.cs
+++ SeedData/Models/Order.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace SeedData.Models
 {
     public class Order
     {
+        [Key]
         public int Id { get; set; }
         public decimal total { get; set; }
         public DateTime orderDate { get; set; }
