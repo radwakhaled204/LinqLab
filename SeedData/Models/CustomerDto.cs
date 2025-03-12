4dbd1425939298e9f7b143052dc0ae79137d78fa
Radwa Khaled <radwaabouzed@gmail.com>
2025-03-12 22:32:54 +0200

    add data annotion

--- SeedData/Models/CustomerDto.cs
+++ SeedData/Models/CustomerDto.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ namespace SeedData.Models
     {
         [Required] 
         public string customerName { get; set; }
+        [Column("Phone")]
         public long customerPhone { get; set; }
     }
 }
