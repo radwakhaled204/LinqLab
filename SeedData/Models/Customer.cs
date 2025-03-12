a9b81119d78a161db0a13f009aecac6952c32115
Radwa Khaled <radwaabouzed@gmail.com>
2025-03-12 22:27:32 +0200

    add data annotion

--- SeedData/Models/Customer.cs
+++ SeedData/Models/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace SeedData.Models
     public class Customer : IComparable<Customer>
     {
         public int id { get; set; }
+        [Required]
         public string name { get; set; }
         public long telephone { get; set; }
         public int age { get; set; }
