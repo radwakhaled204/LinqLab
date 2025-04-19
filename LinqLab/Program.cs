9bed87c04d5bb79e5942f92dd91293dde1181dbd
Radwa Khaled <radwaabouzed@gmail.com>
2025-04-19 13:45:44 +0200

    update

--- LinqLab/Program.cs
+++ LinqLab/Program.cs
@@ -3,22 +3,31 @@ using SeedData;
 using SeedData.Models;
 using System.Linq;
 
+class Program
+{
+    static void Main()
+    {
+        var cats = GetData.GetCategories(GetData.GetCustomers());
 
-var cats = GetData.GetCategories(GetData.GetCustomers());
+        var list = cats.Select(c => c.customer);
+        foreach (var c in list)
+        {
+            foreach (var cust in c)
+            {
+                Console.WriteLine(cust.id + " : " + cust.name);
+            }
+        }
 
-//var list = cats.Select(c => c.customer);
+    }
+
+}
+
+
+
+
+//var list = cats.SelectMany(c => c.customer);
 //foreach (var c in list)
 //{
-//    foreach (var cust in c)
-//    {
-//        Console.WriteLine(cust.id + " : " + cust.name);
-//    }
+//    Console.WriteLine(c.id + " : " + c.name);
 //}
-
-
-var list = cats.SelectMany(c => c.customer);
-foreach (var c in list)
-{
-    Console.WriteLine(c.id + " : " + c.name);
-}
 
